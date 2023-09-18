cec56a49994509bcd031307755ddd6e873f0b4ba
swapna sri vootla <svootla@uncc.edu>
2023-09-17 23:18:39 -0400

    Added D3JS chart

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 const express = require('express');
-const fs = require('fs'); // Import the file system module
+// const fs = require('fs'); // Import the file system module
 const app = express();
 const port = 3000;
-
+const data = require("./budget.json");
 app.use('/', express.static('public'));
 
-// Define the path to your JSON file
-const budgetFilePath = 'budget.json';
-
-
+const budget = { 
+  myBudget: [
+  {
+      title: 'Eat Out',
+      budget: 30
+  },
+  {
+      title: 'Rent',
+      budget: 350
+  },
+  {
+      title: 'Groceries',
+      budget: 90
+  }
+]};
 
 app.get('/budget', (req, res) => {
-  // Read the JSON file and send its contents as the response
-  fs.readFile(budgetFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading JSON file:', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-
-    // Parse the JSON data and send it as the response
-    const budgetData = JSON.parse(data);
-    res.json(budgetData);
-  });
+  res.send(data);
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
